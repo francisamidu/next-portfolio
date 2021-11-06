85719c514c443d513a7bdf4d706cac4aa468a33f
francisamidu <francisamidu124@gmail.com>
2021-11-06 17:38:13 +0200

    Added responsive styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,25 +6,25 @@ import TrendingItem from "./TrendingItem";
 
 const Header = () => {
   return (
-    <section className="flex pt-12 relative flex-col">
+    <section className="flex md:pt-12 pt-4 relative flex-col">
       <TrendingItem />
-      <nav className="flex flex-row items-center md:justify-between justify-around py-2 px-8">
+      <nav className="flex flex-row items-center  justify-between md:px-8 py-2 px-4">
         <Link href="/">
           <a className="text-3xl font-bold text-blue-600">Cryptohub</a>
         </Link>
         <div className="flex flex-row justify-between item-center">
           <Link href="login">
-            <a className="text-1xl text-gray-600 hover:text-gray-400">Login</a>
+            <a className="text-1xl text-gray-600 mr-4 hover:text-gray-400">Login</a>
           </Link>
           <Link href="/signup">
-            <a className="text-1xl text-gray-500"></a>
+            <a className="text-1xl text-gray-600 hover:text-gray-400">Signup</a>
           </Link>
         </div>
       </nav>
-      <div className="px-8 flex flex-row justify-between items-center">
-        <div className="flex flex-col text-gray-500 mr-12 px-8 w-3/4">
-          <h1 className="text-4xl text-blue-900 font-bold mb-4">
-            Cryptocurrency news and data belongs in your dashboard
+      <div className="px-8 mt-12 md:mt-0 flex md:flex-row flex-col justify-between items-center">
+        <div className="flex flex-col md:block text-gray-500 md:w-2/4 w-full md:text-left text-center px-4 md:px-8 items-center">
+          <h1 className="text-4xl text-blue-900 font-bold mb-4 leading-11">
+            Trending Cryptocurrency news belongs in your dashboard
           </h1>
           <p className="leading-7 my-4">
             Cryptohub is the industry-leading dashboard for keeping you up to
@@ -37,8 +37,8 @@ const Header = () => {
         <Image
           src="/bitcoin.jpg"
           alt="Bitcoin illustration"
-          width="1024"
-          height="1024"
+          width="1000"
+          height="960"
           className="w-1/2 h-full"
         />
       </div>
--- components/TrendingItem.jsx
+++ components/TrendingItem.jsx
@@ -6,9 +6,9 @@ import { useNewsContext } from "../contexts/NewsContext";
 const TrendingItem = () => {
   const { trendingNews } = useNewsContext();
   return (
-    <div className="flex flex-row justify-between items-center py-1.5 px-2 absolute w-2/3 text-sm rounded-full bg-blue-100 trending-item rounded-lg">
+    <div className="hidden md:flex flex-row justify-between items-center py-1.5 px-2 absolute w-1/2 text-sm rounded-full bg-blue-100 trending-item rounded-lg">
       <p className="ml-4">{trendingNews?.title}</p>
-      <Link href={trendingNews.link}>
+      <Link href={trendingNews?.link}>
         <a className="w-max px-4 py-2 rounded-full rounded-lg text-white bg-blue-600">
           Learn More
         </a>
